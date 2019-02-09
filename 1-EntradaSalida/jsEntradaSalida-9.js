48238d943933a11e7fa2e84fee229a0d6a2ad7e5
nacho <delvalleigna@hotmail.com>
2019-02-08 22:28:18 -0300

    ej 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,24 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+
+
+	var sueldo11;
+	var aumento;
+	var sueldofinal;
+	var cuadroresultado	
+
+	sueldo11 = sueldo.value;
+	aumento = (sueldo11 * 10 / 100);
+	sueldofinal = (sueldo11 + aumento);
+	cuadroresultado = resultado.value ;
+
+	sueldo11 = parseInt(sueldo11);
+
+
+	
+
+	cuadroresultado ("sueldofinal");
+
 	
 }
