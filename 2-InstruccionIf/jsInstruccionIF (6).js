7574bde832d719df7ee1b093008313c5164323fb
ignaciodelvalle18 <delvalleigna@hotmail.com>
2019-02-16 12:42:23 -0300

    clase apoyo 1.1

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,25 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad18;
+	edad18 = edad.value;
+	edad18 = parseInt(edad18);
 
+	if(edad18>=18) 
+	{
+		alert("sos mayor");
+	}
+	else
+	{
+	
+	if (edad18<=12)
+		alert ("sos adolescente");
+	}
+	else
+	{
+		alert("sos un niño");
+	}
+ 
 
 
 }//FIN DE LA FUNCIÓN
