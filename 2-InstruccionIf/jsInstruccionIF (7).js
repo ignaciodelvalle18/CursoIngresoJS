19bb6562a61578c7b0d3d2b8d3d356ecfa34cc34
ignaciodelvalle18 <47437113+ignaciodelvalle18@users.noreply.github.com>
2019-02-28 14:38:53 -0300

    if

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad1 
+	var estado
+
+ 	edad1 = edad.value
+ 	edad1 = parseInt(edad1)
+ 	estado = estadoCivil.value
+
+
+ 	if (edad1>18&&estado!= "Soltero")
+ 	{
+ 		alert("Es muy pequeño para NO ser soltero.")
+ 	} 
 
 	
 
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -3,6 +3,19 @@ function mostrar()
 //tomo la edad  
 
 	
+var edad1 
+	var estado
+
+ 	edad1 = edad.value
+ 	edad1 = parseInt(edad1)
+ 	estado = estadoCivil.value
+
+
+ 	if (edad1<18&&estado = "Soltero")
+ 	{
+ 		alert("Es soltero y no es menor.")
+ 	} 
+ 	else
 
 
 }//FIN DE LA FUNCIÓN
