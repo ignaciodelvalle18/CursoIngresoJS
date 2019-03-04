28431a10c57f3130387e330031a9ba26db8b1c44
nachito <delvalleigna@hotmail.com>
2019-03-04 02:11:53 -0300

    parcial 4.1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,41 +1,34 @@
 function mostrar()
 {
-	//si la suma es mayor a 10 indicarlo x alert
-
 	var numero1;
 	var numero2;
 	var concatenados;
 	var resta;
 	var suma;
 
-	numero1 = prompt("Ingrese un numero");
-	numero2= prompt("Ingrese el segundo numero");
-	numero1 = parseInt(numero1);
-	numero2 = parseInt(numero2);
+	numero1=prompt("Ingrese un numero");
+	numero2=prompt("Ingrese el segundo numero");
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
 
 	if(numero1==numero2)
 	{
-		concatenados = numero1 + numero2;
-		alert("Su numero concatenado es " + concatenados);
-
+		concatenados=numero1+numero2;
+		mensaje="Su numero concatenado es "+numero1+numero2;
 	}
-
 	else if(numero1>numero2)
 	{
-		resta = numero1 - numero2;
-		alert("La resta es " + resta);
+		resta=numero1-numero2;
+		mensaje="La resta es "+resta;
 	}
 	else 
 	{
-		suma = numero1 + numero2;
-		
+		suma=numero1+numero2;
+		mensaje="La suma es "+suma;
 	if(suma>=10)
 			{
-				alert("La suma es " + suma + " y supero el 10");
-				break;
+				mensaje="La suma es "+suma+" y supero el 10";
 			}
-			alert("La suma es "+ suma);
-
 	}
-
+	alert(mensaje);
 }
