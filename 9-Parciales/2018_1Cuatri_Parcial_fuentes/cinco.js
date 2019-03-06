33b2ac304984291659a5b88335ca213853d46b48
nachito <delvalleigna@hotmail.com>
2019-03-06 03:27:09 -0300

    hh

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,5 +1,41 @@
 function mostrar()
 {
-dsd
+
+	var sistemasolar;
+	var mensaje;
+
+	sistemasolar = prompt("Ingrese un planeta del sistema solar");
+
+	switch(sistemasolar)
+	{
+		case "mercurio":
+		mensaje="acá hace más calor";
+		break;
+		case "venus":
+		mensaje="acá hace más calor";
+		break;
+		case "tierra":
+		mensaje="acá vivimos";
+		break;
+		case "marte":
+		mensaje="acá hace más calor";
+		break;
+		case "júpiter":
+		mensaje="acá hace más frio";
+		break;
+		case "saturno":
+		mensaje="acá hace más frio";
+		break;
+		case "urano":
+		mensaje="acá hace más frio";
+		break;
+		case "neptuno":
+		mensaje="acá hace más frio";
+		break;
+		default:
+			mensaje = "ERROR No ingresó un planeta válido";
+			break;
+	}
+	alert(mensaje);
 
 }
